708fabfbdc55dd9193d48cebd3362c0f26e05444
Connal Meinertz <connal.meinertz@outlook.com>
2026-02-03 14:12:06 -0600

    Added files, fixed gravity, and added sprint with shift

---
+++ utils/physics.h
@@ -0,0 +1,10 @@
+#pragma once
+
+constexpr float gravity = 1250;
+constexpr float damping = 0.88;
+constexpr float walk_acceleration = 2300;
+constexpr float jump_velocity = 700;
+constexpr float terminal_velocity = 10000;
+
+class Physics {
+};
